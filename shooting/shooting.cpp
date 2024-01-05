35088fd7f97b9558f5d6c8776959b6e538b6d267
ThomasLix7 <hongtaolee@hotmail.com>
2024-01-05 19:34:05 +0100

    working on a shooting game

---
+++ shooting/shooting.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+#include <chrono>
+using namespace std;
+
+#include <Windows.h>;
+
+
+int nScreenWidth = 120;
+int nScreenHeight = 40;
+
+float fPlayerX = 8.0f;
+float fPlayerY = 8.0f;
+float fPlayerA = 0.0f;
+
+int nMapHeight = 16;
+int nMapWidth = 16;
+
+float fFOV = 3.14159 / 4.0;
+float fDepth = 16.0f;
+
+int main() {
+	wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(hConsole);
+	DWORD dwBytesWritten = 0;
+
+	wstring map;
+
+	map += L"################";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"################";
+
+
+	auto tp1 = chrono::system_clock::now();
+	auto tp2 = chrono::system_clock::now();
+
+	while (1) {
+
+		tp2 = chrono::system_clock::now();
+		chrono::duration<float> elapsedTime = tp2 - tp1;
+		tp1 = tp2;
+		float fElapsedTime = elapsedTime.count();
+
+
+		// Controlls
+		// Handle CCW Rotation
+		if (GetAsyncKeyState((unsigned short)'A') & 0x8000)
+			fPlayerA -= (0.8f) * fElapsedTime;
+
+		if (GetAsyncKeyState((unsigned short)'D') & 0x8000)
+			fPlayerA += (0.8f) * fElapsedTime;
+
+		if (GetAsyncKeyState((unsigned short)'W') & 0x8000) {
+			fPlayerX += sinf(fPlayerA) * 5.0f * fElapsedTime;
+			fPlayerY += cosf(fPlayerA) * 5.0f * fElapsedTime;
+		}
+			
+		if (GetAsyncKeyState((unsigned short)'S') & 0x8000) {
+			fPlayerX -= sinf(fPlayerA) * 5.0f * fElapsedTime;
+			fPlayerY -= cosf(fPlayerA) * 5.0f * fElapsedTime;
+		}
+
+		for (int x = 0; x < nScreenWidth; x++) {
+			// For each column, calculate the projected ray angle into world space
+			float fRayAngle = (fPlayerA - fFOV / 2.0f) + ((float)x / (float)nScreenWidth) * fFOV;
+
+			float fDistanceToWall = 0;
+			bool bHitWall = false;
+
+			float fEyeX = sinf(fRayAngle); // Unit vector for ray in player space
+			float fEyeY = cosf(fRayAngle);
+
+			while (!bHitWall && fDistanceToWall < fDepth) {
+
+
+				fDistanceToWall += 0.1f;
+
+				int nTestX = (int)(fPlayerX + fEyeX * fDistanceToWall);
+				int nTestY = (int)(fPlayerY + fEyeY * fDistanceToWall);
+
+				// Test if ray is our of bounds
+				if (nTestX < 0 || nTestX >= nMapWidth || nTestY < 0 || nTestY >= nMapHeight) {
+					bHitWall = true;      // Just set distance to maximum depth
+					fDistanceToWall = fDepth;
+				}
+				else {
+					// Ray is inbounds so test to see if the ray cell is a wall block
+					if (map[nTestY * nMapWidth + nTestX] == '#') {
+						bHitWall = true;
+					}
+				}
+			}
+
+			// Calculate distance to ceiling and floor
+			int nCeiling = (float)(nScreenHeight / 2.0) - nScreenHeight / ((float)fDistanceToWall);
+			int nFloor = nScreenHeight - nCeiling;
+
+			short nShade = ' ';
+
+			if (fDistanceToWall <= fDepth / 4.0f)     nShade = 0x2588;    // very close
+			else if (fDistanceToWall < fDepth / 3.0f) nShade = 0x2593;
+			else if (fDistanceToWall < fDepth / 2.0f) nShade = 0x2592;
+			else if (fDistanceToWall < fDepth)        nShade = 0x2591;
+			else                                      nShade = ' ';
+
+			for (int y = 0; y < nScreenHeight; y++) {
+				if (y < nCeiling)
+					screen[y * nScreenWidth + x] = ' ';
+				else if (y > nCeiling && y <= nFloor)
+					screen[y * nScreenWidth + x] = nShade;
+				else
+					screen[y * nScreenWidth + x] = ' ';
+			}
+		}
+		screen[nScreenWidth * nScreenHeight - 1] = '\0';
+		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+	}
+
+
+	return 0;
+}
